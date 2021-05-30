a7402bb57dabd2a9fb9e0f8eaa5aabc1bdf1e1b8
moonsung1234 <dit67890@gmail.com>
2021-05-31 01:40:32 +0900

    .

---
+++ SortAndNavigation/palindrome.py
@@ -0,0 +1,20 @@
+
+def palindrome(_str) :
+    queue = [] # FIFO : First In First Out
+    stack = [] # LIFO : Last In First Out
+
+    for s in _str :
+        if s.isalpha() :
+            queue.append(s.lower())
+            stack.append(s.lower())
+
+    while queue and stack :
+        if not queue.pop(0) == stack.pop() :
+            return False
+
+    return True
+
+print(palindrome("Wow"))
+print(palindrome("HelloWorld!"))
+
+
